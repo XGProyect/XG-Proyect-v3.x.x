408d6c9a33a1f3d458a01be071670bc0a9d81072
Lucas Kovács <lucas.kovacs@gmail.com>
2016-04-07 12:43:23 -0300

    Fixed issue #67
    Actually this feature is being disabled for now.

--- src/xgp3.0.0/upload/application/views/adm/users_moons_table_view.php
+++ src/xgp3.0.0/upload/application/views/adm/users_moons_table_view.php
@@ -1,20 +1,20 @@
 <tr>
-	<td width="50%">
-		{moon_name} {moon_status}<br />
-		<img src="{image_path}{moon_image}.jpg" alt="{moon_image}.jpg" title="{moon_image}.jpg" border="0" {moon_image_style}>
-	</td>
-	<td width="50%">
-		<div class="btn-group">
-			<button class="btn btn-info dropdown-toggle" data-toggle="dropdown">{us_user_planets_actions} <span class="caret"></span></button>
-			<ul class="dropdown-menu">
-				<li><a href="admin.php?page=users&type=moons&edit=moon&user={user}&moon={moon_id}">{us_user_moons_edit}</a></li>
-				<li><a href="admin.php?page=users&type=moons&edit=buildings&user={user}&moon={moon_id}">{us_user_buildings_edit}</a></li>
-				<li><a href="admin.php?page=users&type=moons&edit=ships&user={user}&moon={moon_id}">{us_user_ships_edit}</a></li>
-				<li><a href="admin.php?page=users&type=moons&edit=defenses&user={user}&moon={moon_id}">{us_user_defenses_edit}</a></li>
-				<li class="divider"></li>
-				<li><a href="admin.php?page=users&type=moons&edit=delete&dltmode=soft&user={user}&moon={moon_id}">{us_user_delete_moon} {us_user_delete_pm_soft}</a></li>
-                                <li><a href="admin.php?page=users&type=moons&edit=delete&dltmode=physical&user={user}&moon={moon_id}">{us_user_delete_moon} {us_user_delete_pm_physical}</a></li>
-			</ul>
-		</div>
-	</td>
+    <td width="50%">
+        {moon_name} {moon_status}<br />
+        <img src="{image_path}{moon_image}.jpg" alt="{moon_image}.jpg" title="{moon_image}.jpg" border="0" {moon_image_style}>
+    </td>
+    <td width="50%">
+        <div class="btn-group">
+            <button class="btn btn-info dropdown-toggle" data-toggle="dropdown">{us_user_planets_actions} <span class="caret"></span></button>
+            <ul class="dropdown-menu">
+                <li><a href="admin.php?page=users&type=moons&edit=moon&user={user}&moon={moon_id}">{us_user_moons_edit}</a></li>
+                <li><a href="admin.php?page=users&type=moons&edit=buildings&user={user}&moon={moon_id}">{us_user_buildings_edit}</a></li>
+                <li><a href="admin.php?page=users&type=moons&edit=ships&user={user}&moon={moon_id}">{us_user_ships_edit}</a></li>
+                <li><a href="admin.php?page=users&type=moons&edit=defenses&user={user}&moon={moon_id}">{us_user_defenses_edit}</a></li>
+                <li class="divider"></li>
+                <li><a href="admin.php?page=users&type=moons&edit=delete&dltmode=soft&user={user}&moon={moon_id}">{us_user_delete_moon} {us_user_delete_pm_soft}</a></li>
+                <!--<li><a href="admin.php?page=users&type=moons&edit=delete&dltmode=physical&user={user}&moon={moon_id}">{us_user_delete_moon} {us_user_delete_pm_physical}</a></li>-->
+            </ul>
+        </div>
+    </td>
 </tr>
