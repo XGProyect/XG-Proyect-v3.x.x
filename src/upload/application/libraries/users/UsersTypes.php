4a8879847ba5241e3ce7478643083efd9d7aaa18
Lucas Kovács <lucas.kovacs@gmail.com>
2017-10-06 18:25:58 -0300

    Added UsersTypes class enumerator

---
+++ src/upload/application/libraries/users/UsersTypes.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Users Library
+ *
+ * PHP Version 5.5+
+ *
+ * @category Library
+ * @package  Application
+ * @author   XG Proyect Team
+ * @license  http://www.xgproyect.org XG Proyect
+ * @link     http://www.xgproyect.org
+ * @version  3.0.4
+ */
+
+namespace application\libraries\users;
+
+/**
+ * UsersTypes Class
+ *
+ * @category Enumerator
+ * @package  Libraries
+ * @author   XG Proyect Team
+ * @license  http://www.xgproyect.org XG Proyect
+ * @link     http://www.xgproyect.org
+ * @version  3.1.0
+ */
+abstract class UsersTypes
+{
+    const player    = 0;
+    const go        = 1;
+    const sgo       = 2;
+    const admin     = 3;
+}
+
+/* end of MessagesTypes.php */
