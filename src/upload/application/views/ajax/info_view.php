184226f4f7f4b0da02f189e83b63ad293d167df2
Lucas Kovács <lucas.kovacs@gmail.com>
2017-10-14 14:59:54 -0300

    Proper HTML format applied

--- src/upload/application/views/ajax/info_view.php
+++ src/upload/application/views/ajax/info_view.php
@@ -1,46 +1,46 @@
 <div class="inner-box clearfix">
-	<h2>{aj_info_about_title}</h2>
-	<p>{aj_info_about_description}</p>
+    <h2>{aj_info_about_title}</h2>
+    <p>{aj_info_about_description}</p>
 </div>
 <div class="inner-box clearfix">
-	<h2>{aj_info_features}</h2>
-	<ul>
-		<li>{aj_info_description_line1}</li>
-		<li>{aj_info_description_line2}</li>
-		<li>{aj_info_description_line3}</li>
+    <h2>{aj_info_features}</h2>
+    <ul>
+        <li>{aj_info_description_line1}</li>
+        <li>{aj_info_description_line2}</li>
+        <li>{aj_info_description_line3}</li>
         <li>{aj_info_description_line4}</li>
         <li>{aj_info_description_line5}</li>
         <li>{aj_info_description_line6}</li>
         <li>{aj_info_description_line7}</li>
         <li>{aj_info_description_line8}</li>
-	</ul>
+    </ul>
     <a class="overlay button" href="index.php?page=team" title="{aj_info_team}">{aj_info_team}</a> <a class="overlay button" href="index.php?page=credits" title="{aj_info_credits}">{aj_info_credits}</a>
 </div>
 <div class="inner-box last clearfix">
-	<h2>{aj_info_images}</h2>
-	<div id="screens">
-	</div>
-	<br class="clearfloat" />
+    <h2>{aj_info_images}</h2>
+    <div id="screens">
+    </div>
+    <br class="clearfloat" />
 </div>
 <script type="text/javascript">
-$(document).ready( function() {
-    //GALLERY Fancybox
-	$('#ajaxContent a.overlay').fancybox({
-		'onStart':	function() {
-						$.validationEngine.closePrompt('.formError',true);
-					},
-		type: 'iframe',
-		'hideOnContentClick': true ,
-		height: 433,
-		width: 480
+    $(document).ready(function () {
+        //GALLERY Fancybox
+        $('#ajaxContent a.overlay').fancybox({
+            'onStart': function () {
+                $.validationEngine.closePrompt('.formError', true);
+            },
+            type: 'iframe',
+            'hideOnContentClick': true,
+            height: 433,
+            width: 480
 
+        });
+        $('#screens a').fancybox({
+            'overlayColor': '#000',
+            'hideOnContentClick': true,
+            'onStart': function () {
+                $.validationEngine.closePrompt('.formError', true);
+            }
+        });
     });
-	$('#screens a').fancybox({
-		'overlayColor': '#000',
-		'hideOnContentClick': true,
-		'onStart':	function() {
-	    				$.validationEngine.closePrompt('.formError',true);
-					}
-	});
-});
 </script>
--- src/upload/application/views/ajax/media_view.php
+++ src/upload/application/views/ajax/media_view.php
@@ -1,28 +1,28 @@
 
 <html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><style type="text/css"></style></head><body><div class="inner-box clearfix">
-    <h2>{aj_media_wallpapers}</h2>
-	<div id="wallpapers" class="clearfix">
-    </div>
-</div>
-<div class="inner-box last clearfix">
-	<h2>{aj_media_pictures}</h2>
-	<div id="screens">
-    </div>
-</div>
-<div class="inner-box last clearfix">
-    <h2>{aj_media_concept_art}</h2>
-    <div id="screens">
-    </div>
-</div>
-<script type="text/javascript">
-$(document).ready( function() {
-//GALLERY Fancybox
-	$('#wallpapers a.zoom, #screens a').fancybox({
-		'overlayColor': '#000',
-		'hideOnContentClick': true,
-		'onStart':	function() {
-	    				$.validationEngine.closePrompt('.formError',true);
-					}
-	});
-});
-</script></body></html>
+            <h2>{aj_media_wallpapers}</h2>
+            <div id="wallpapers" class="clearfix">
+            </div>
+        </div>
+        <div class="inner-box last clearfix">
+            <h2>{aj_media_pictures}</h2>
+            <div id="screens">
+            </div>
+        </div>
+        <div class="inner-box last clearfix">
+            <h2>{aj_media_concept_art}</h2>
+            <div id="screens">
+            </div>
+        </div>
+        <script type="text/javascript">
+            $(document).ready(function () {
+                //GALLERY Fancybox
+                $('#wallpapers a.zoom, #screens a').fancybox({
+                    'overlayColor': '#000',
+                    'hideOnContentClick': true,
+                    'onStart': function () {
+                        $.validationEngine.closePrompt('.formError', true);
+                    }
+                });
+            });
+        </script></body></html>
