6960a3c8b785e9a97dbd79eeeac178e12db9521a
Lucas Kovács <lucas.kovacs@gmail.com>
2018-06-04 17:14:31 +0200

    Added validation to avoid admin panel blockage when game disable

--- src/upload/application/libraries/FunctionsLib.php
+++ src/upload/application/libraries/FunctionsLib.php
@@ -721,7 +721,9 @@ abstract class FunctionsLib extends XGPCore
      */
     public static function checkServer($current_user)
     {
-        if (self::readConfig('game_enable') == 0 && $current_user['user_authlevel'] < UserRanksEnumerator::admin) {
+        if (self::readConfig('game_enable') == 0 
+            && $current_user['user_authlevel'] < UserRanksEnumerator::admin 
+            && !defined('IN_ADMIN')) {
 
             self::message(stripslashes(FunctionsLib::readConfig('close_reason')), '', '', false, false);
             die();
