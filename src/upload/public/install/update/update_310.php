54e7a8743d374d4037d7ce5e56c78426efeae324
Lucas Kovács <lucas.kovacs@gmail.com>
2018-06-07 14:20:29 +0200

    - Added buddy table update.-

--- src/upload/public/install/update/update_310.php
+++ src/upload/public/install/update/update_310.php
@@ -1,2 +1,3 @@
 <?php
 $queries[] = "DELETE FROM `" . OPTIONS . "` WHERE `option_name` = 'ssl_enabled'";
+$queries[] = "ALTER TABLE `" . BUDDY . "` ADD PRIMARY KEY (`buddy_id`), ADD KEY `buddy_id` (`buddy_id`);";
