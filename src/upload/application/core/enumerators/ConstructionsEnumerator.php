8e4f0b93c0d073cd41164107cc490262e535669b
Lucas Kovács <lucas.kovacs@gmail.com>
2018-08-09 22:21:08 +0300

    + new enumerators

---
+++ src/upload/application/core/enumerators/ConstructionsEnumerator.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Constructions enumerator
+ *
+ * PHP Version 5.5+
+ *
+ * @category Library
+ * @package  Application
+ * @author   XG Proyect Team
+ * @license  http://www.xgproyect.org XG Proyect
+ * @link     http://www.xgproyect.org
+ * @version  3.1.0
+ */
+namespace application\core\enumerators;
+
+/**
+ * ConstructionsEnumerator Class
+ *
+ * @category Enumerator
+ * @package  Core
+ * @author   XG Proyect Team
+ * @license  http://www.xgproyect.org XG Proyect
+ * @link     http://www.xgproyect.org
+ * @version  3.1.0
+ */
+abstract class ConstructionsEnumerator
+{
+    const building_metal_mine = 1;
+    const building_crystal_mine = 2;
+    const building_deuterium_sintetizer = 3;
+    const building_solar_plant = 4;
+    const building_fusion_reactor = 12;
+    const building_robot_factory = 14;
+    const building_nano_factory = 15;
+    const building_hangar = 21;
+    const building_metal_store = 22;
+    const building_crystal_store = 23;
+    const building_deuterium_tank = 24;
+    const building_laboratory = 31;
+    const building_terraformer = 33;
+    const building_ally_deposit = 34;
+    const building_mondbasis = 41;
+    const building_phalanx = 42;
+    const building_jump_gate = 43;
+    const building_missile_silo = 44;
+
+}
+
+/* end of ConstructionsEnumerator.php */
