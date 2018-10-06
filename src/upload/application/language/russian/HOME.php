d8cc23cb31e902f138d3973808079fb68b23f409
blackmcs <webwork.lds@gmail.com>
2018-10-06 11:24:04 +0300

    Перевод на руский язык

---
+++ src/upload/application/language/russian/HOME.php
@@ -0,0 +1,111 @@
+<?php
+
+// index
+$lang['hm_keywords']						= 'Game, Browser, online, for free, legendary, MMOG, Science fiction, space, space ship';
+$lang['hm_description']						= 'XG Proyect - The legendary game in the space! Discover the universe together with thousands of players.';
+$lang['hm_field_required']					= 'This field is required';
+$lang['hm_must_accept_tandc']				= 'You must accept the T&Cs.';
+$lang['hm_username_length']					= 'Between 3 and 20 characters allowed.';
+$lang['hm_password_length']					= 'Between 8 and 20 characters allowed.';
+$lang['hm_valid_email_address']				= 'You need to enter a valid e-mail-address!';
+$lang['hm_not_valid_characters']			= 'Contains invalid characters.';
+$lang['hm_username_underscore']				= 'Your name may not start or end with an underscore.';
+$lang['hm_username_space']					= 'Your name may not start or end with a space.';
+$lang['hm_username_many_underscore']		= 'Your name may not contain more than 3 underscores in total.';
+$lang['hm_username_many_spaces']			= 'Your name may not include more than 3 spaces in total.';
+$lang['hm_username_underscore_continued']	= 'You may not use two or more underscores one after the other.';
+$lang['hm_username_spaces_continued']		= 'You may not use two or more spaces one after the other.';
+$lang['hm_username_available']				= 'This username is available.';
+$lang['hm_username_loading']				= 'Please wait, loading...';
+$lang['hm_username_not_available']			= 'This username is not available anymore';
+$lang['hm_only_characters']					= 'Use characters only.';
+$lang['hm_hidden_title']					= 'XGProyect - Conquer the universe';
+$lang['hm_login_button']					= 'Login';
+$lang['hm_close_button']					= 'Cerrar';
+$lang['hm_universe']						= 'Universe:';
+$lang['hm_username']						= 'Email / Username:';
+$lang['hm_password']						= 'Password:';
+$lang['hm_password_forgot']					= 'Forgot your password?';
+$lang['hm_play_for_free']					= 'PLAY FOR FREE!';
+$lang['hm_terms_accept']					= 'With the login I accept the';
+$lang['hm_terms']							= 'T&C`s';
+$lang['hm_mail_address']					= 'E-Mail-Address:';
+$lang['hm_password_level']					= 'Password safety:';
+$lang['hm_accept']							= 'I accept the';
+$lang['hm_and']								= 'and';
+$lang['hm_policy']							= 'Privacy Policy';
+$lang['hm_register']						= 'Register';
+$lang['hm_home']							= 'Home';
+$lang['hm_about']							= 'About XGP';
+$lang['hm_media']							= 'Media';
+$lang['hm_forum']							= 'Board';
+
+// ajax - home
+$lang['aj_home_conquer_universe']			= 'XG Proyect - Conquer the universe';
+$lang['aj_home_description']				= '<em>XG Proyect</em> is a strategy-game set in space. Thousands of players across the world compete at the same time. In order to play you only need a web browser.';
+$lang['aj_home_rules']						= 'Rules';
+$lang['aj_home_trailer']					= 'Trailer';
+
+// ajax - about
+$lang['aj_info_about_title']				= 'XG Proyect is a game of intergalactic conquest.';
+$lang['aj_info_about_description']			= '<em>XG Proyect</em> is a strategy-game set in space. Thousands of players across the world compete at the same time. In order to play you only need a web browser.';
+$lang['aj_info_features']					= 'Features';
+$lang['aj_info_description_line1']			= 'You start out with just one undeveloped world and turn that into a <em>mighty empire</em> able to defend your hard earned colonies.';
+$lang['aj_info_description_line2']			= 'Create an <em>economic and military infrastructure</em> to support your quest for the next greatest technological achievements.';
+$lang['aj_info_description_line3']			= '<em>Wage war</em> against other empires as you struggle with other players to gain the materials.';
+$lang['aj_info_description_line4']			= '<em>Negotiate</em> with other emperors and create an alliance or trade for much needed resources.';
+$lang['aj_info_description_line5']			= '<em>Build an armada</em> to enforce your will throughout the universe.';
+$lang['aj_info_description_line6']			= '<em>Hoard your resources</em> behind an impregnable wall of planetary defences.';
+$lang['aj_info_description_line7']			= 'Whatever you wish to do, <em>XG Proyect can let you do it.</em>';
+$lang['aj_info_description_line8']			= 'Will you terrorize the area around you? Or will you strike fear into the hearts of those who attack the helpless?';
+$lang['aj_info_team']						= 'Team';
+$lang['aj_info_credits']					= 'Credits';
+$lang['aj_info_images']						= 'Pictures';
+
+// ajax - media
+$lang['aj_media_wallpapers']				= 'Wallpapers';
+$lang['aj_media_pictures']					= 'Pictures';
+$lang['aj_media_concept_art']				= 'Concept Art';
+
+// registration
+$lang['re_disabled']						= 'New user registration is not currently available.';
+$lang['re_mail_text_part1']					= 'Welcome to XG Proyect, thanks for joining our game!<br/> In this email you\'ll fin all the information that you need to login into your account.';
+$lang['re_mail_text_part2']					= '<strong>Data to access the game</strong>';
+$lang['re_mail_text_part3']					= 'Your username';
+$lang['re_mail_text_part4']					= 'Your password';
+$lang['re_mail_text_part5']					= 'Game link';
+$lang['re_mail_text_part6']					= 'Enjoy the game!';
+$lang['re_mail_text_part7']					= 'Regards,<br/>The XGP Team.-';
+$lang['re_mail_register_at']				= 'Joined to XG Proyect';
+
+// message for new users
+$lang['re_welcome_message_from']			= 'Admin';
+$lang['re_welcome_message_subject']			= 'Welcome to XG Proyect!';
+$lang['re_welcome_message_content']			= 'Greetings, emperor %s!
+<br/><br/>
+I offer you the grandest of congratulations on entering into your regency and also I hope to be of help to you in your first steps.
+<br/><br/>
+On the left, you will see the menu, which will help you to control the fortunes of your galactic empire.
+<br/><br/>
+I`m sure you already know the `overview`. Under `Resources` and `Facilities` you can build the constructions you need to support your empire. Get the best start by improving your Solar Plant, so that you acquire energy and can supply the mines with this energy.
+<br/><br/>
+Subsequently, improve your metal and crystal mines to start producing the necessary resources. You can also just take a look around the menus, you will certainly be able to find your way around quite quickly.
+<br/><br/>
+More help, tips and tactics can be found here:
+<br/><br/>
+The tutorial: <a href="game.php?page=tutorial">tutorial</a><br/>
+IRC Java chat: <a href="game.php?page=irc">IRC chat</a><br/>
+The forum: <a href="game.php?page=forum">OGame boards</a><br/>
+Support: <a href="game.php?page=support">game support</a><br/>
+<br/><br/>
+
+The latest announcements and changes in the game can only be found in the forum. There, on the bar at the top of the screen, you will find the `Chat` button. This takes you directly to the IRC chat. The channel selection list contains the #ogame-support channel, where you can ask questions directly. The Game Operator in charge of your Universe can also be found there.
+<br/><br/>
+Now you are fully armed for the future - good luck!
+<br/><br/>
+This message will be deleted in 24 hours.';
+
+// general
+$lang['ge_home_planet']						= 'Homeworld';
+$lang['ge_colony']							= 'Colony';
+/* end of HOME.php */
