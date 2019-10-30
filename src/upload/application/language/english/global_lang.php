b43bf0910ea44e2ceace726f8bef71674ab70603
Lucas Kovacs <lucas.kovacs@gmail.com>
2019-10-30 16:52:40 +0100

    + global language lines

---
+++ src/upload/application/language/english/global_lang.php
@@ -0,0 +1,10 @@
+<?php
+$lang = [
+    // resources
+    'metal' => "Metal",
+    'crystal' => "Crystal",
+    'deuterium' => "Deuterium",
+    "dark_matter" => "Dark Matter",
+];
+
+/* end of global_lang.php */
---
+++ src/upload/application/language/spanish/global_lang.php
@@ -0,0 +1,10 @@
+<?php
+$lang = [
+    // resources
+    'metal' => "Metal",
+    'crystal' => "Cristal",
+    'deuterium' => "Deuterio",
+    "dark_matter" => "Materia Oscura",
+];
+
+/* end of global_lang.php */
