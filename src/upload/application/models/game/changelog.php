46014a44c1579f772d324937eef7fdfc7adde74a
Lucas Kovacs <lucas.kovacs@gmail.com>
2019-11-04 14:45:40 +0100

    - fixed issue #276

--- src/upload/application/models/game/changelog.php
+++ src/upload/application/models/game/changelog.php
@@ -61,11 +61,11 @@ class Changelog
                 c.`changelog_version`,
                 c.`changelog_date`,
                 c.`changelog_description`
-            FROM `xgp_changelog` c
-            LEFT JOIN `xgp_languages` l
+            FROM `" . CHANGELOG . "` c
+            LEFT JOIN `" . LANGUAGES . "` l
                 ON l.`language_id` = c.`changelog_lang_id`
             WHERE l.`language_name` = (
-                SELECT o.`option_value` FROM `xgp_options` o WHERE `option_name` = 'lang'
+                SELECT o.`option_value` FROM `" . OPTIONS . "` o WHERE `option_name` = 'lang'
             )
             ORDER BY c.`changelog_date` DESC"
         );
