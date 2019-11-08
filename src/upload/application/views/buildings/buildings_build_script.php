e03af6a02b1934444690c7a4ac1a9f37540a37fc
Lucas Kovács <lucas.kovacs@gmail.com>
2019-11-08 09:02:23 +0100

    - adjusted scripts to refresh after 1 second

--- src/upload/application/views/buildings/buildings_build_script.php
+++ src/upload/application/views/buildings/buildings_build_script.php
@@ -10,6 +10,7 @@ function t(){
 	h  = 0;
 	if ( s < 0 ) {
 		brp.innerHTML = '{bd_ready}<br><a href=game.php?page={call_program}>{bd_continue}</a>';
+		window.setTimeout('document.location.href="game.php?page={call_program}";', 1000);
 	} else {
 		if ( s > 59 ) { m = Math.floor( s / 60 ); s = s - m * 60; }
 		if ( m > 59 ) { h = Math.floor( m / 60 ); m = m - h * 60; }
