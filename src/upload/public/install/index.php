68c93929a6a0a76b735af54cbad3c9296556bc10
Lucas Kovács <lucas.kovacs@gmail.com>
2019-11-14 14:24:57 +0100

    - PSR2

--- src/upload/public/install/index.php
+++ src/upload/public/install/index.php
@@ -17,11 +17,10 @@ define('XGP_ROOT', '../../');
 
 require XGP_ROOT . 'application' . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'common.php';
 
-$page       = isset($_GET['page']) ? $_GET['page'] : 'installation';
-$file_name  = XGP_ROOT . INSTALL_PATH . $page . '.php';
+$page = isset($_GET['page']) ? $_GET['page'] : 'installation';
+$file_name = XGP_ROOT . INSTALL_PATH . $page . '.php';
 
 if (file_exists($file_name)) {
-
     include $file_name;
 
     $class_name = 'application\controllers\install\\' . ucfirst($page);
