3b5535d906dff76889869503fb156a140b7c621c
Lucas Kovács <lucas.kovacs@gmail.com>
2019-11-17 17:55:44 +0100

    - administration refactor

---
+++ src/upload/application/language/spanish/adm/navigation_lang.php
@@ -0,0 +1,13 @@
+<?php
+$lang = [
+    'nv_search_for' => 'Buscar...',
+    'nv_alert_center' => 'Centro de alertas',
+    'nv_new_download' => '¡Hay una nueva versión disponible!',
+    'nv_logout' => 'Cerrar sesión',
+    'nv_ready_to_leave' => '¿Listo para partir?',
+    'nv_ready_to_leave_instructions' => 'Seleccione "Cerrar sesión" a continuación si está listo para finalizar su sesión actual.',
+    'nv_ready_to_leave_cancel' => 'Cancelar',
+    'nv_ready_to_leave_logout' => 'Cerrar sesión',
+];
+
+/* end of navigation_lang.php */
---
+++ src/upload/application/models/adm/login.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Login Model
+ *
+ * PHP Version 7.1+
+ *
+ * @category Model
+ * @package  Application
+ * @author   XG Proyect Team
+ * @license  http://www.xgproyect.org XG Proyect
+ * @link     http://www.xgproyect.org
+ * @version  3.1.0
+ */
+namespace application\models\adm;
+
+/**
+ * Login Class
+ *
+ * @category Classes
+ * @package  Application
+ * @author   XG Proyect Team
+ * @license  http://www.xgproyect.org XG Proyect
+ * @link     http://www.xgproyect.org
+ * @version  3.1.0
+ */
+class Login
+{
+
+    private $db = null;
+
+    /**
+     * Constructor
+     *
+     * @return void
+     */
+    public function __construct($db)
+    {
+        // use this to make queries
+        $this->db = $db;
+    }
+
+    /**
+     * __destruct
+     *
+     * @return void
+     */
+    public function __destruct()
+    {
+        $this->db->closeConnection();
+    }
+
+    /**
+     * Get user data to login
+     *
+     * @param string $user_email
+     * @param string $user_password
+     * @return array
+     */
+    public function getLoginData(string $user_email, string $user_password): array
+    {
+        $result = $this->db->queryFetch(
+            "SELECT
+                `user_id`,
+                `user_name`,
+                `user_password`
+            FROM `" . USERS . "`
+            WHERE `user_email` = '" . $this->db->escapeValue($user_email) . "'
+                AND `user_password` = '" . sha1($user_password) . "'
+                AND `user_authlevel` >= '1'
+            LIMIT 1"
+        );
+
+        if ($result) {
+            return $result;
+        }
+
+        return [];
+    }
+}
+
+/* end of login.php */
