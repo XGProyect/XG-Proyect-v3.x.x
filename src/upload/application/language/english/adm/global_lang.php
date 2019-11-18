e7f1fec2cc956177a9f8108b15693e3ccc09a5e8
Lucas Kovács <lucas.kovacs@gmail.com>
2019-11-18 19:21:24 +0100

    - admin refactor

--- src/upload/application/language/english/adm/global_lang.php
+++ src/upload/application/language/english/adm/global_lang.php
@@ -1,6 +1,12 @@
 <?php
 $lang = [
     'no_permissions' => 'You need special permissions to see this page',
+    'user_level' => [
+        0 => 'Player',
+        1 => 'Operator (GO)',
+        2 => 'Super Operator (SGo)',
+        3 => 'Administrator (GA)',
+    ],
 ];
 
 /* end of global_lang.php */
--- src/upload/application/language/spanish/adm/global_lang.php
+++ src/upload/application/language/spanish/adm/global_lang.php
@@ -1,6 +1,12 @@
 <?php
 $lang = [
     'no_permissions' => 'Necesitas permisos especiales para ver esta página',
+    'user_level' => [
+        0 => 'Jugador',
+        1 => 'Operador (GO)',
+        2 => 'Super Operador (SGo)',
+        3 => 'Administrador (GA)',
+    ],
 ];
 
 /* end of global_lang.php */
