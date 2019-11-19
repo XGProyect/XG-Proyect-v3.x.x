9e5b99b8795d6af9ca55f8dbb99b86dc31beba54
Lucas Kovács <lucas.kovacs@gmail.com>
2019-11-19 23:10:55 +0100

    - admin refactor

---
+++ src/upload/application/language/english/adm/fleets_lang.php
@@ -0,0 +1,35 @@
+<?php
+$lang = [
+    'ff_title' => 'Fleet movements',
+    'ff_sub_title' => 'Here you can get an overview of all the fleet movements taking place on the server, you can also execute different actions over them.',
+    'ff_general' => 'General',
+    'ff_mission' => 'Mission',
+    'ff_ammount' => 'Ships (total)',
+    'ff_beginning' => 'Beginning',
+    'ff_departure' => 'Departure',
+    'ff_objective' => 'Destiny',
+    'ff_arrival' => 'Objective',
+    'ff_return' => 'Return',
+    'ff_actions' => 'Actions',
+    'ff_ships' => 'Ships',
+    'ff_r' => '(R)',
+    'ff_a' => '(A)',
+    'ff_metal' => 'Metal',
+    'ff_crystal' => 'Crystal',
+    'ff_deuterium' => 'Deuterium',
+    'ff_no_resources' => 'No resources',
+    'ff_type_mission' => [
+        1 => 'Attack',
+        2 => 'ACS Attack',
+        3 => 'Transport',
+        4 => 'Deploy',
+        5 => 'ACS Defense',
+        6 => 'Espionage',
+        7 => 'Colonize',
+        8 => 'Recycle',
+        9 => 'Destroy',
+        15 => 'Expedition',
+    ],
+];
+
+/* end of fleets_lang.php */
---
+++ src/upload/application/language/spanish/adm/fleets_lang.php
@@ -0,0 +1,35 @@
+<?php
+$lang = [
+    'ff_title' => 'Flotas en vuelo',
+    'ff_sub_title' => 'Aquí tendras un vistazo general de todos los movimientos de flota que están ocurriendo en el servidor, además puedes realizar diferentes acciones sobre ellas.',
+    'ff_general' => 'General',
+    'ff_mission' => 'Misión',
+    'ff_ammount' => 'Cantidad',
+    'ff_beginning' => 'Comienzo',
+    'ff_departure' => 'Salida',
+    'ff_objective' => 'Destino',
+    'ff_arrival' => 'Objetivo',
+    'ff_return' => 'Retorno',
+    'ff_actions' => 'Acciones',
+    'ff_ships' => 'Naves',
+    'ff_r' => '(R)',
+    'ff_a' => '(A)',
+    'ff_metal' => 'Metal',
+    'ff_crystal' => 'Cristal',
+    'ff_deuterium' => 'Deuterio',
+    'ff_no_resources' => 'Sin recursos',
+    'ff_type_mission' => [
+        1 => 'Atacar',
+        2 => 'Ataq. confederación',
+        3 => 'Transportar',
+        4 => 'Desplegar',
+        5 => 'Mantener posición',
+        6 => 'Espiar',
+        7 => 'Colonizar',
+        8 => 'Reciclar campo de escombros',
+        9 => 'Destruir',
+        15 => 'Expedición',
+    ],
+];
+
+/* end of fleets_lang.php */
