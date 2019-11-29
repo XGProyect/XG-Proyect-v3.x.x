f09252f9507cc72fa2c42595147543f7d80728fe
Lucas Kovács <lucas.kovacs@gmail.com>
2019-11-29 16:06:55 +0100

    - admin refactor

---
+++ src/upload/application/views/adm/footer_view.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Powered by XG Proyect&reg; Version {version} Copyright &copy; {year}</span>
+        </div>
+    </div>
+</footer>
