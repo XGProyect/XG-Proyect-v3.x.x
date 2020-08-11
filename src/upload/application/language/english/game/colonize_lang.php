70e7c3c2d4af76d30d6dec8d92587a59a94d6352
Lucas Kovacs <lucas.kovacs@gmail.com>
2020-08-11 22:27:59 +0200

    - better language lines

--- src/upload/application/language/english/game/colonize_lang.php
+++ src/upload/application/language/english/game/colonize_lang.php
@@ -1,11 +1,11 @@
 <?php
 $lang = [
-    'col_max_colonies' => 'The settlers arrived at the coordinates %s, but, unfortunately, can not colonize, can have no more than %s planets.',
-    'col_successful' => 'The settlers arrived at the coordinates %s, the settlers are beginning to build a new colony.',
-    'col_occupied' => 'The settlers arrived at the coordinates %s, settlers would not have found a planet with these details. They are forced to turn back completely demoralized ...',
-    'col_astro_level' => 'The settlers arrived at the coordinates %s, but is not possible because you do not have the required level of astrophysics.',
-    'col_report_from' => 'Colonization',
-    'col_report_title' => 'Report of settlement',
+    'col_max_colonies' => 'The fleet has arrived at assigned coordinates %s, but, unfortunately, can not colonize, can have no more than %s planets.',
+    'col_successful' => 'The fleet has arrived at assigned coordinates %s, the settlers are beginning to build a new colony.',
+    'col_occupied' => 'The fleet has arrived at assigned coordinates %s, settlers would not have found a planet with these details. They are forced to turn back completely demoralized ...',
+    'col_astro_level' => 'The fleet has arrived at assigned coordinates %s and ascertains that the planet is viable for colonisation. Shortly after starting to develop the planet, the colonists realise that their knowledge of astrophysics is not sufficient to complete the colonisation of a new planet.',
+    'col_report_from' => 'Settlers',
+    'col_report_title' => 'Settlement Report',
 ];
 
 /* end of colonize_lang.php */
