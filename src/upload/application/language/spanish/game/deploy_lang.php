5918b20f60615b7c6781a185dc1d52e529ab257a
Lucas Kovacs <lucas.kovacs@gmail.com>
2020-08-12 22:29:49 +0200

    - deployment mission refactor

---
+++ src/upload/application/language/spanish/game/deploy_lang.php
@@ -0,0 +1,7 @@
+<?php
+$lang = [
+    'dep_report_title' => 'Despliegue de flota',
+    'dep_report_deployed' => 'Una de tus flotas de %s %s ha alcanzado %s %s. La flota entrega %s Metal, %s Cristal y %s Deuterio.',
+];
+
+/* end of deploy_lang.php */
