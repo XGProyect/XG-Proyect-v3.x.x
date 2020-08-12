07e468e047c2516fa23a11f1bcfc0290453349f2
Lucas Kovacs <lucas.kovacs@gmail.com>
2020-08-12 10:03:09 +0200

    - language refactor

--- src/upload/application/language/english/game/colonize_lang.php
+++ src/upload/application/language/english/game/colonize_lang.php
@@ -1,9 +1,9 @@
 <?php
 $lang = [
     'col_max_colonies' => 'The fleet has arrived at assigned coordinates %s, but, unfortunately, can not colonize, can have no more than %s planets.',
-    'col_successful' => 'The fleet has arrived at assigned coordinates %s, the settlers are beginning to build a new colony.',
+    'col_successful' => 'The fleet has arrived at assigned coordinates %s, found a new planet there and are beginning to develop upon it immediately.', // ok
     'col_occupied' => 'The fleet has arrived at assigned coordinates %s, settlers would not have found a planet with these details. They are forced to turn back completely demoralized ...',
-    'col_astro_level' => 'The fleet has arrived at assigned coordinates %s and ascertains that the planet is viable for colonisation. Shortly after starting to develop the planet, the colonists realise that their knowledge of astrophysics is not sufficient to complete the colonisation of a new planet.',
+    'col_astro_level' => 'The fleet has arrived at assigned coordinates %s and ascertains that the planet is viable for colonisation. Shortly after starting to develop the planet, the colonists realise that their knowledge of astrophysics is not sufficient to complete the colonisation of a new planet.', // ok
     'col_report_from' => 'Settlers',
     'col_report_title' => 'Settlement Report',
 ];
--- src/upload/application/language/spanish/game/combatreport_lang.php
+++ src/upload/application/language/spanish/game/combatreport_lang.php
@@ -5,21 +5,21 @@ $lang = [
     'cr_first_round' => '(La flota fue destruida en la primer ronda)',
     'cr_type' => 'Tipo',
     'cr_total' => 'Total',
-    'cr_technologies' => 'Armas: %d%% Escudos: %d%% Casco: %d%%',
+    'cr_technologies' => 'Armas: %d%% Escudos: %d%% Casco: %d%%', // ok
     'cr_destroyed' => '¡Destruida!',
     'cr_no_access' => '¡El reporte solicitado no existe!',
     'cr_attacker_lostunits' => 'El atacante ha perdido un total de %d unidades.',
     'cr_defender_lostunits' => 'El defensor ha perdido un total de %d unidades.',
-    'cr_fleet_attack_1' => 'El Atacante dispara un total de %d veces sobre el Defensor con una fuerza conjunta de %d.',
-    'cr_fleet_attack_2' => 'Los escudos del defensor absorben %d puntos de daño.',
-    'cr_fleet_defs_1' => 'El Defensor dispara un total de %d veces sobre el Atacante con una fuerza conjunta de %d.',
-    'cr_fleet_defs_2' => 'Los escudos del atacante absorben %d puntos de daño.',
+    'cr_fleet_attack_1' => 'El Atacante dispara un total de %d veces sobre el Defensor con una fuerza conjunta de %d.', // ok
+    'cr_fleet_attack_2' => 'Los escudos del defensor absorben %d puntos de daño.', // ok
+    'cr_fleet_defs_1' => 'El Defensor dispara un total de %d veces sobre el Atacante con una fuerza conjunta de %d.', // ok
+    'cr_fleet_defs_2' => 'Los escudos del atacante absorben %d puntos de daño.', // ok
     'cr_debris_units' => 'En estas coordenadas flotan %d unidades de metal y %d unidades de cristal',
     'cr_moonproba' => 'La probabilidad de que surja una luna de los escombros es de %d %%',
     'cr_attacker_won' => 'El atacante ha ganado la batalla',
     'cr_defender_won' => 'El defensor ha ganado la batalla',
     'cr_both_won' => 'La batalla termino en empate',
-    'cr_stealed_ressources' => 'Botín total: %d metal, %d cristal y %d deuterio.',
+    'cr_stealed_ressources' => 'Botín total: %d metal, %d cristal y %d deuterio.', // ok
     'cr_moonbuilt' => 'Las enormes cantidad de metal y cristal se fusionan y forman un satélite lunar en la orbita del planeta %d [%d:%d:%d]!',
 ];
 
