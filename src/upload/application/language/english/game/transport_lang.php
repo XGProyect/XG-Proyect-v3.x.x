227d2be31bbe41651f8ab060bd1aba59839b0ae3
Lucas Kovacs <lucas.kovacs@gmail.com>
2020-08-12 13:46:15 +0200

    - fixes

--- src/upload/application/language/english/game/transport_lang.php
+++ src/upload/application/language/english/game/transport_lang.php
@@ -1,7 +1,7 @@
 <?php
 $lang = [
     'tra_reaching' => 'Reaching a planet', // ok
-    'tra_delivered_resources' => 'Your fleet from planet %s %s has reached the planet %s %s and delivered its goods:<br>Metal: %d Crystal: %d Deuterium: %d.', // ok
+    'tra_delivered_resources' => 'Your fleet from planet %s %s has reached the planet %s %s and delivered its goods:<br>Metal: %s Crystal: %s Deuterium: %s.', // ok
 ];
 
 /* end of transport_lang.php */
--- src/upload/application/language/spanish/game/transport_lang.php
+++ src/upload/application/language/spanish/game/transport_lang.php
@@ -1,7 +1,7 @@
 <?php
 $lang = [
     'tra_reaching' => 'Alcanzando un planeta',
-    'tra_delivered_resources' => 'Tu flota del planeta %s %s alcanza el planet %s %s y entrega su mercancía:<br>Metal: %d Cristal: %d Deuterio: %d.',
+    'tra_delivered_resources' => 'Tu flota del planeta %s %s alcanza el planet %s %s y entrega su mercancía:<br>Metal: %s Cristal: %s Deuterio: %s.',
 ];
 
 /* end of transport_lang.php */
