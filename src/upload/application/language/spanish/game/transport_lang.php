2ccca6382a1f7f5b6224d449d194fa65740f3f31
Lucas Kovacs <lucas.kovacs@gmail.com>
2020-08-12 18:34:23 +0200

    - language improvements

--- src/upload/application/language/spanish/game/transport_lang.php
+++ src/upload/application/language/spanish/game/transport_lang.php
@@ -1,7 +1,10 @@
 <?php
 $lang = [
-    'tra_reaching' => 'Alcanzando un planeta',
+    'tra_reaching' => 'Llegada a un planeta', // ok
     'tra_delivered_resources' => 'Tu flota del planeta %s %s alcanza el planet %s %s y entrega su mercancía:<br>Metal: %s Cristal: %s Deuterio: %s.',
+    'tra_incoming_from' => 'Monitoreo espacial',
+    'tra_incoming_title' => 'Entrega de recursos por flota extranjera',
+    'tra_incoming_delivery' => 'Una flota de %s (%s %s) está entregando recursos a %s %s: <br>Metal:%s Cristal:%s Deuterio:%s <br>Antes tenías: Metal:%s Cristal:%s Deuterio:%s<br>Ahora tienes: Metal:%s Cristal:%s Deuterio:%s',
 ];
 
 /* end of transport_lang.php */
