a3b593169fb7f7fb064ec99ffbc22f05f4066bdf
Lucas Kovacs <lucas.kovacs@gmail.com>
2020-08-13 09:25:09 +0200

    - language refactor

---
+++ src/upload/application/language/english/game/destroy_lang.php
@@ -0,0 +1,14 @@
+<?php
+$lang = [
+    'des_report_title' => 'Destruction Report',
+    'des_report_start' => 'A fleet from planet %s [%d:%d:%d] goes to planet moon [%d:%d:%d]',
+    'des_report_attacker' => 'The Death Stars shoot the graviton over the moon orbit.',
+    'des_report_defender' => 'The defender has failed to stop the destruction of the moon.',
+    'des_result_none' => 'The Death Stars didn\'t generate enough power, the mission failed and the ships returned.',
+    'des_result_moon' => 'The tremors began to shake the surface of the moon, after a few moments the moon couldn\'t longer resist and blows into thousand of pieces, mission accomplished, the fleet returns to the planet of origin.',
+    'des_result_ds' => 'The tremors began to shake the surface of the moon, but something goes wrong, the Death Stars graviton also causes tremors making the Death Stars blow into thousand of pieces.',
+    'des_moon_chances' => 'The probability of destroying the moon is: %d %% ',
+    'des_ds_chances' => 'The probability that the stars of death are destroyed is: %d %% ',
+];
+
+/* end of destroy_lang.php */
---
+++ src/upload/application/language/spanish/game/destroy_lang.php
@@ -0,0 +1,14 @@
+<?php
+$lang = [
+    'des_report_title' => 'Reporte de Destruccion',
+    'des_report_start' => 'Una flota del planeta %s [%d:%d:%d] se dirige a la luna del planeta [%d:%d:%d]',
+    'des_report_attacker' => 'Las estrellas de la muerte disparan el graviton a la órbita de la luna.',
+    'des_report_defender' => 'El defensor ha conseguido detener la destrucción de la luna.',
+    'des_result_none' => 'Las estrellas de la muerte no generan el poder necesario, la mision falla y las naves vuelven de regreso.',
+    'des_result_moon' => 'Los temblores empiezan a sacudir la superficie de la luna, despues de unos instantes la luna no soporta mas y vuela en mil pedazos, mision cumplida, la flota vuelve al planeta de origen.',
+    'des_result_ds' => 'Los temblores empiezan a sacudir la superficie de la luna, pero algo sale mal, el graviton en las estrellas de la muerte también provoca temblores y las estrellas de la muerte vuelan en mil pedazos.',
+    'des_moon_chances' => 'La probabilidad de destruir la luna es de: %d %% ',
+    'des_ds_chances' => 'La probabilidad de que sean destruidas las estrellas de la muerte es de: %d %% ',
+];
+
+/* end of destroy_lang.php */
