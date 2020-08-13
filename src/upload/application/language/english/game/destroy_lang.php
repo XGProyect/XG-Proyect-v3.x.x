6ce532bfc6293b59a1779aca06fec3ad1785f564
Lucas Kovacs <lucas.kovacs@gmail.com>
2020-08-13 13:40:51 +0200

    - language refactor

--- src/upload/application/language/english/game/destroy_lang.php
+++ src/upload/application/language/english/game/destroy_lang.php
@@ -1,14 +1,13 @@
 <?php
 $lang = [
-    'des_report_title' => 'Destruction Report',
-    'des_report_start' => 'A fleet from planet %s [%d:%d:%d] goes to planet moon [%d:%d:%d]',
-    'des_report_attacker' => 'The Death Stars shoot the graviton over the moon orbit.',
+    'des_report_title' => 'Moon attack', // ok
+    'des_report_start' => 'Your fleet from %s [%d:%d:%d] has arrived on the moon of the planet %s [%d:%d:%d].', // ok
     'des_report_defender' => 'The defender has failed to stop the destruction of the moon.',
-    'des_result_none' => 'The Death Stars didn\'t generate enough power, the mission failed and the ships returned.',
-    'des_result_moon' => 'The tremors began to shake the surface of the moon, after a few moments the moon couldn\'t longer resist and blows into thousand of pieces, mission accomplished, the fleet returns to the planet of origin.',
+    'des_result_none' => 'The structure of the moon was not sufficiently weakened, and the fleet is returning to its home planet.', // ok
+    'des_result_moon' => 'The weapons of the deathstar fire an alternating graviton pshock at the moon, building up to a massive quake and finally tearing the satellite apart. All buildings on the moon were destroyed. A complete success!<br><br>The fleet is returning to its home planet.<br>', // ok
     'des_result_ds' => 'The tremors began to shake the surface of the moon, but something goes wrong, the Death Stars graviton also causes tremors making the Death Stars blow into thousand of pieces.',
-    'des_moon_chances' => 'The probability of destroying the moon is: %d %% ',
-    'des_ds_chances' => 'The probability that the stars of death are destroyed is: %d %% ',
+    'des_result_both' => 'Your deathstar aim its alternating graviton pshock cannon at the satellite. At first, light quakes incresingly build up on the surface of the moon. The moon begins to deform and is torn apart. Gigantic chunks of rubble come flying towards your fleet. It is too late to evade them. Your entire fleet is obliterated in a hail of rubble.', // ok
+    'des_moon_ds_chances' => 'Moon destruction chance: %d %%, DS destruction chance:%d %%', // ok
 ];
 
 /* end of destroy_lang.php */
