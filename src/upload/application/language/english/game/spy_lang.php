96eb01ade739eef5b72529e2e558e883af9414c9
Lucas Kovacs <lucas.kovacs@gmail.com>
2020-08-16 11:10:34 +0200

    - improved spy language lines

--- src/upload/application/language/english/game/spy_lang.php
+++ src/upload/application/language/english/game/spy_lang.php
@@ -7,9 +7,9 @@ $lang = [
     'spy_report_buildings' => 'Building', // ok
     'spy_report_research' => 'Research', // ok
     'spy_report_detection' => 'Chance of counter-espionage: %d%%', // ok
-    'spy_activity_title' => 'Espionage activity on %s %s',
-    'spy_activity_from' => 'Space control',
-    'spy_activity_enemy_seen' => 'A fleet from planet %s %s (%s) has been detected near your planet %s %s.<br>The probability of counter-espionage is %s%%',
+    'spy_activity_title' => 'Espionage action on %s %s', // ok
+    'spy_activity_from' => 'Space Monitoring', // ok
+    'spy_activity_enemy_seen' => 'A foreign fleet from planet %s %s (%s) was sighted near your planet %s %s.<br>Chance of counter-espionage: %s%%', // ok
     'spy_result_destroyed_title' => 'Contact was lost with the attacking fleet.<br>%s',
     'spy_result_destroyed' => '(The fleet was destroyed in the first round)',
 ];
