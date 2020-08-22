5bc8c7bffa5793395456c2dfb2814f87c7da03f0
Lucas Kovacs <lucas.kovacs@gmail.com>
2020-08-22 17:29:11 +0200

    - completed spanish lines

--- src/upload/application/language/spanish/game/transport_lang.php
+++ src/upload/application/language/spanish/game/transport_lang.php
@@ -1,10 +1,10 @@
 <?php
 $lang = [
     'tra_reaching' => 'Llegada a un planeta', // ok
-    'tra_delivered_resources' => 'Tu flota del planeta %s %s alcanza el planeta %s %s y entrega su mercancía:<br>Metal: %s Cristal: %s Deuterio: %s.',
-    'tra_incoming_from' => 'Monitoreo espacial',
-    'tra_incoming_title' => 'Entrega de recursos por flota extranjera',
-    'tra_incoming_delivery' => 'Una flota de %s (%s %s) está entregando recursos a %s %s: <br>Metal:%s Cristal:%s Deuterio:%s <br>Antes tenías: Metal:%s Cristal:%s Deuterio:%s<br>Ahora tienes: Metal:%s Cristal:%s Deuterio:%s',
+    'tra_delivered_resources' => 'Tu flota de %s %s ha llegado al planeta %s %s y ha entregado su mercancía:<br>Metal: %s Cristal: %s Deuterio: %s.', // ok - added "de %s %s" so it makes sense. There's a bug on the original game
+    'tra_incoming_from' => 'Control del espacio', // ok
+    'tra_incoming_title' => 'Entrega de recursos por flota ajena', // ok
+    'tra_incoming_delivery' => 'Una flota de %s (%s %s) entrega recursos a %s %s : <br>Metal:%s Cristal:%s Deuterio:%s <br>Hasta ahora tenías: Metal:%s Cristal:%s Deuterio:%s<br>Ahora tienes: Metal:%s Cristal:%s Deuterio:%s', // ok
 ];
 
 /* end of transport_lang.php */
