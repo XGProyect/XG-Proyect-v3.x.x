7c9a8fe0a78f5d9741f72c24bcee132c6de065c0
duhow <contacto@duhowpi.net>
2020-10-25 12:39:36 +0100

    Fix PHPUnit bootstrap file

--- src/upload/tests/bootstrap.php
+++ src/upload/tests/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-include_once('../application/core/AutoLoader.php');
+include_once(__DIR__ . '/../application/core/AutoLoader.php');
 
 // Register the directory to your include files
-AutoLoader::registerDirectory('../application/libraries');
+AutoLoader::registerDirectory(__DIR__ . '/../application/libraries');
