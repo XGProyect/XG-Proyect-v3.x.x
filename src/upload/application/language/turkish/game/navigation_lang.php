32518250b98eecbe90777867823b426fbd7000b0
Ufuk Avcıoğlu <ufuk.avcioglu@hotmail.com.tr>
2020-11-08 18:51:59 +0300

    officier update

--- src/upload/application/language/turkish/game/navigation_lang.php
+++ src/upload/application/language/turkish/game/navigation_lang.php
@@ -1,17 +1,5 @@
 <?php
 $lang = [
-    'tn_hire_commander' => 'Commander edin',
-    'tn_hire_admiral' => 'Amiral görevlendir',
-    'tn_hire_engineer' => 'Mühendis görevlendir',
-    'tn_hire_geologist' => 'Jeolog görevlendir',
-    'tn_hire_technocrat' => 'Teknokrat görevlendir',
-    'tn_add_premium_officier_commander' => '+40 favori, inşa listesi, imparatorluk menüsü, kısa yollar, nakliye tarayıcısı, reklamsız oyun* (*oyunlarla ilgili notlar hariç)',
-    'tn_add_premium_officier_admiral' => 'Maksimum filo sayısı +2, maksimum keşif sayısı +1, geliştirilmiş filo kaçış oranı',
-    'tn_add_premium_officier_engineer' => 'Savunmada yarı yarıya düşüş, +%10 enerji üretimi',
-    'tn_add_premium_officier_geologist' => '+10% maden geliri',
-    'tn_add_premium_officier_technocrat' => '+2 casusluk seviyesi, %25 daha az araştırma süresi',
-    'tn_time_remaining_one' => '%d gün kaldı',
-    'tn_time_remaining_many' => '%d gün daha aktif',
     'tn_vacation_mode' => 'Tatil modu şu tarihe kadar aktiftir: ',
     'tn_vacation_mode_active' => 'Tatil modu aktif',
     'tn_delete_mode' => 'Hesabınız silinmek üzere ayarlandı. Silme tarihi: ',
