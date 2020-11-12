80906b299084ad85c285e9173a526fc946d792d4
Lucas Kovacs <lucas.kovacs@gmail.com>
2020-11-12 19:49:57 +0100

    #399 fixed wrong query

--- src/upload/application/models/game/overview.php
+++ src/upload/application/models/game/overview.php
@@ -29,8 +29,8 @@ class Overview extends Model
     {
         if ((int) $user_id > 0) {
             return $this->db->queryFetchAll(
-                "SELECT
-                    f.*,
+                "SELECT DISTINCT
+                    fleets.*,
                     po.`planet_name` AS `start_planet_name`,
                     pt.`planet_name` AS `target_planet_name`,
                     uo.`user_name` AS `start_planet_user`,
@@ -39,38 +39,48 @@ class Overview extends Model
                         SELECT
                             GROUP_CONCAT(am.`acs_user_id`)
                         FROM `" . ACS_MEMBERS . "` am
-                        WHERE am.`acs_group_id` = f.fleet_group
+                        WHERE am.`acs_group_id` = fleets.`fleet_group`
                     ) AS `acs_members`
-                FROM `" . FLEETS . "` f
-                    INNER JOIN `" . USERS . "` uo
-                    	ON uo.`user_id` = f.`fleet_owner`
-                    LEFT JOIN `" . USERS . "` ut
-                    	ON ut.`user_id` = f.`fleet_target_owner`
-                    LEFT JOIN `" . ACS . "` acs
-                	ON
-                        (
-                            acs.acs_galaxy = f.fleet_end_galaxy AND
-                            acs.acs_system = f.fleet_end_system AND
-                            acs.acs_planet = f.fleet_end_planet AND
-                            acs.acs_planet_type = f.fleet_end_type
-                        )
-                    INNER JOIN `" . PLANETS . "` po
-                	    ON (
-                            po.planet_galaxy = f.fleet_start_galaxy AND
-                            po.planet_system = f.fleet_start_system AND
-                            po.planet_planet = f.fleet_start_planet AND
-                            po.planet_type = f.fleet_start_type
-                    )
-                    LEFT JOIN `" . PLANETS . "` pt
-                	    ON (
-                            pt.planet_galaxy = f.fleet_end_galaxy AND
-                            pt.planet_system = f.fleet_end_system AND
-                            pt.planet_planet = f.fleet_end_planet AND
-                            pt.planet_type = f.fleet_end_type
-                    )
-                WHERE (f.`fleet_owner` = '" . $user_id . "'
-                    OR f.`fleet_target_owner` = '" . $user_id . "')
-                    OR f.`fleet_group` = acs.`acs_id`"
+                FROM
+                (
+                    SELECT
+                        f.*
+                    FROM
+                        `" . FLEETS . "` f
+                    WHERE
+                        f.`fleet_owner` = '" . $user_id . "'
+                    OR
+                        f.`fleet_target_owner` = '" . $user_id . "'
+                    UNION ALL
+                    SELECT
+                        f.*
+                    FROM
+                        `" . ACS_MEMBERS . "` am
+                    LEFT JOIN `" . FLEETS . "` f ON
+                        f.`fleet_group` = am.`acs_group_id`
+                    WHERE
+                        f.`fleet_id` IS NOT NULL
+                    AND
+                        am.`acs_user_id` = '" . $user_id . "'
+                ) fleets
+                INNER JOIN `" . USERS . "` uo ON
+                    uo.`user_id` = fleets.`fleet_owner`
+                LEFT JOIN `" . USERS . "` ut ON
+                    ut.`user_id` = fleets.`fleet_target_owner`
+                INNER JOIN `" . PLANETS . "` po ON
+                (
+                    po.`planet_galaxy` = fleets.`fleet_start_galaxy` AND
+                    po.`planet_system` = fleets.`fleet_start_system` AND
+                    po.`planet_planet` = fleets.`fleet_start_planet` AND
+                    po.`planet_type` = fleets.`fleet_start_type`
+                )
+                LEFT JOIN `" . PLANETS . "` pt ON
+                (
+                    pt.`planet_galaxy` = fleets.`fleet_end_galaxy` AND
+                    pt.`planet_system` = fleets.`fleet_end_system` AND
+                    pt.`planet_planet` = fleets.`fleet_end_planet` AND
+                    pt.`planet_type` = fleets.`fleet_end_type`
+                )"
             );
         }
 
