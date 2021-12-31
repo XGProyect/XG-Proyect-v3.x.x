807c4e68e550c922ead2642a28ed2170db5c90f3
Lucas Kovács <lucas.kovacs@gmail.com>
2021-12-31 16:56:01 +0100

    deprecation fixes

--- src/upload/app/ThirdParty/battle_engine/tests/runnable/HTMLPurifier/HTMLPurifier.autoload.php
+++ src/upload/app/ThirdParty/battle_engine/tests/runnable/HTMLPurifier/HTMLPurifier.autoload.php
@@ -13,10 +13,6 @@ if (function_exists('spl_autoload_register') && function_exists('spl_autoload_un
         // Be polite and ensure that userland autoload gets retained
         spl_autoload_register('__autoload');
     }
-} elseif (!function_exists('__autoload')) {
-    function __autoload($class) {
-        return HTMLPurifier_Bootstrap::autoload($class);
-    }
 }
 
 if (ini_get('zend.ze1_compatibility_mode')) {
--- src/upload/app/ThirdParty/battle_engine/tests/runnable/HTMLPurifier/HTMLPurifier/TagTransform/Font.php
+++ src/upload/app/ThirdParty/battle_engine/tests/runnable/HTMLPurifier/HTMLPurifier/TagTransform/Font.php
@@ -64,7 +64,7 @@ class HTMLPurifier_TagTransform_Font extends HTMLPurifier_TagTransform
         if (isset($attr['size'])) {
             // normalize large numbers
             if ($attr['size'] !== '') {
-                if ($attr['size']{0} == '+' || $attr['size']{0} == '-') {
+                if ($attr['size'][0] == '+' || $attr['size'][0] == '-') {
                     $size = (int) $attr['size'];
                     if ($size < -2) $attr['size'] = '-2';
                     if ($size > 4)  $attr['size'] = '+4';
