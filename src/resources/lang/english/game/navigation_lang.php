65402092d0b43f25654643de569af088b2cbdba0
José Manuel Delicado Alcolea <jmdaweb@hotmail.com>
2022-08-08 20:30:16 +0200

    Added 5 missing strings in languages

--- src/resources/lang/english/game/navigation_lang.php
+++ src/resources/lang/english/game/navigation_lang.php
@@ -4,4 +4,9 @@ $lang = [
     'tn_vacation_mode' => 'Vacation mode active until ',
     'tn_vacation_mode_active' => 'Vacation mode active',
     'tn_delete_mode' => 'Your account was set for deletion. Deletion date: ',
+    'tn_commander' => 'commander',
+    'tn_admiral' => 'admiral',
+    'tn_engineer' => 'engineer',
+    'tn_geologist' => 'geologist',
+    'tn_technocrat' => 'technocrat',
 ];
--- src/resources/lang/spanish/game/navigation_lang.php
+++ src/resources/lang/spanish/game/navigation_lang.php
@@ -4,4 +4,9 @@ $lang = [
     'tn_vacation_mode' => 'Modo vacaciones activo hasta el ',
     'tn_vacation_mode_active' => 'Modo vacaciones activo',
     'tn_delete_mode' => 'Tu cuenta se encuentra en modo borrar. La misma será borrada el ',
+    'tn_commander' => 'comandante',
+    'tn_admiral' => 'almirante',
+    'tn_engineer' => 'ingeniero',
+    'tn_geologist' => 'geólogo',
+    'tn_technocrat' => 'tecnócrata',
 ];
