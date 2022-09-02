b73f73e15fe5653f03011a2cb3c3dc1414f11db6
José Manuel Delicado Alcolea <jmdaweb@hotmail.com>
2022-09-02 09:12:08 +0200

    Provide more information by default in the alt texts of the devris and moon blocks

--- src/resources/views/galaxy/galaxy_debris_block.php
+++ src/resources/views/galaxy/galaxy_debris_block.php
@@ -1,3 +1,3 @@
 <a style="cursor: pointer;" onmouseover='return overlib("<table width=240><tr><td class=c colspan=2>{gl_debris_field} [{galaxy}:{system}:{planet}]</td></tr><tr><th width=80><img src={image} height=75 width=75 alt=""/></th><th><table><tr><td class=c colspan=2>{gl_resources}:</td></tr><tr><th>{metal}: </th><th>{planet_debris_metal}</th></tr><tr><th>{crystal}: </th><th>{planet_debris_crystal}</th></tr><tr><td class=c colspan=2>{gl_actions}:</td></tr><tr><th colspan=2 align=left><a href= # onclick=&#039javascript:doit (8, {galaxy}, {system}, {planet}, {planettype}, {recsended}); return nd();&#039 >{gl_collect}</a></tr></table></th></tr></table>", STICKY, MOUSEOFF, DELAY, 750, CENTER, OFFSETX, -40, OFFSETY, -40);' onmouseout='return nd();'>
-    <img src="{image}" height="22px" width="22px" alt=""/>
+    <img src="{image}" height="22px" width="22px" alt="{metal}: {planet_debris_metal}, {crystal}: {planet_debris_crystal}"/>
 </a>
