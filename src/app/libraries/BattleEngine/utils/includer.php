61a64b51c2a9f2c1450cf0b0708a58cc20391763
José Manuel Delicado Alcolea <jmdaweb@hotmail.com>
2022-09-18 13:36:38 +0200

    Fixed a bug which caused some classes to be declared twice. Now, includer.php will check wether OPBEPath is defined or not, and include files consequently

--- src/app/libraries/BattleEngine/utils/includer.php
+++ src/app/libraries/BattleEngine/utils/includer.php
@@ -25,30 +25,31 @@
  * @version beta(26-10-2013)
  * @link https://github.com/jstar88/opbe
  */
-define('OPBEPATH', dirname(__DIR__) . DIRECTORY_SEPARATOR);
-
-require(OPBEPATH . 'utils' . DIRECTORY_SEPARATOR . 'GeometricDistribution.php');
-require(OPBEPATH . 'utils' . DIRECTORY_SEPARATOR . 'Gauss.php');
-require(OPBEPATH . 'utils' . DIRECTORY_SEPARATOR . 'DebugManager.php');
-require(OPBEPATH . 'utils' . DIRECTORY_SEPARATOR . 'IterableUtil.php');
-require(OPBEPATH . 'utils' . DIRECTORY_SEPARATOR . 'Math.php');
-require(OPBEPATH . 'utils' . DIRECTORY_SEPARATOR . 'Number.php');
-require(OPBEPATH . 'utils' . DIRECTORY_SEPARATOR . 'Events.php');
-require(OPBEPATH . 'utils' . DIRECTORY_SEPARATOR . 'Lang.php');
-require(OPBEPATH . 'utils' . DIRECTORY_SEPARATOR . 'LangManager.php');
-require(OPBEPATH . 'models' . DIRECTORY_SEPARATOR . 'Type.php');
-require(OPBEPATH . 'models' . DIRECTORY_SEPARATOR . 'ShipType.php');
-require(OPBEPATH . 'models' . DIRECTORY_SEPARATOR . 'Fleet.php');
-require(OPBEPATH . 'models' . DIRECTORY_SEPARATOR . 'HomeFleet.php');
-require(OPBEPATH . 'models' . DIRECTORY_SEPARATOR . 'Defense.php');
-require(OPBEPATH . 'models' . DIRECTORY_SEPARATOR . 'Ship.php');
-require(OPBEPATH . 'models' . DIRECTORY_SEPARATOR . 'Player.php');
-require(OPBEPATH . 'models' . DIRECTORY_SEPARATOR . 'PlayerGroup.php');
-require(OPBEPATH . 'combatObject' . DIRECTORY_SEPARATOR . 'Fire.php');
-require(OPBEPATH . 'combatObject' . DIRECTORY_SEPARATOR . 'PhysicShot.php');
-require(OPBEPATH . 'combatObject' . DIRECTORY_SEPARATOR . 'ShipsCleaner.php');
-require(OPBEPATH . 'combatObject' . DIRECTORY_SEPARATOR . 'FireManager.php');
-require(OPBEPATH . 'core' . DIRECTORY_SEPARATOR . 'Battle.php');
-require(OPBEPATH . 'core' . DIRECTORY_SEPARATOR . 'BattleReport.php');
-require(OPBEPATH . 'core' . DIRECTORY_SEPARATOR . 'Round.php');
-require(OPBEPATH . 'constants' . DIRECTORY_SEPARATOR . 'battle_constants.php');
+if (!defined('OPBEPATH')){
+    define('OPBEPATH', dirname(__DIR__) . DIRECTORY_SEPARATOR);
+    require(OPBEPATH . 'utils' . DIRECTORY_SEPARATOR . 'GeometricDistribution.php');
+    require(OPBEPATH . 'utils' . DIRECTORY_SEPARATOR . 'Gauss.php');
+    require(OPBEPATH . 'utils' . DIRECTORY_SEPARATOR . 'DebugManager.php');
+    require(OPBEPATH . 'utils' . DIRECTORY_SEPARATOR . 'IterableUtil.php');
+    require(OPBEPATH . 'utils' . DIRECTORY_SEPARATOR . 'Math.php');
+    require(OPBEPATH . 'utils' . DIRECTORY_SEPARATOR . 'Number.php');
+    require(OPBEPATH . 'utils' . DIRECTORY_SEPARATOR . 'Events.php');
+    require(OPBEPATH . 'utils' . DIRECTORY_SEPARATOR . 'Lang.php');
+    require(OPBEPATH . 'utils' . DIRECTORY_SEPARATOR . 'LangManager.php');
+    require(OPBEPATH . 'models' . DIRECTORY_SEPARATOR . 'Type.php');
+    require(OPBEPATH . 'models' . DIRECTORY_SEPARATOR . 'ShipType.php');
+    require(OPBEPATH . 'models' . DIRECTORY_SEPARATOR . 'Fleet.php');
+    require(OPBEPATH . 'models' . DIRECTORY_SEPARATOR . 'HomeFleet.php');
+    require(OPBEPATH . 'models' . DIRECTORY_SEPARATOR . 'Defense.php');
+    require(OPBEPATH . 'models' . DIRECTORY_SEPARATOR . 'Ship.php');
+    require(OPBEPATH . 'models' . DIRECTORY_SEPARATOR . 'Player.php');
+    require(OPBEPATH . 'models' . DIRECTORY_SEPARATOR . 'PlayerGroup.php');
+    require(OPBEPATH . 'combatObject' . DIRECTORY_SEPARATOR . 'Fire.php');
+    require(OPBEPATH . 'combatObject' . DIRECTORY_SEPARATOR . 'PhysicShot.php');
+    require(OPBEPATH . 'combatObject' . DIRECTORY_SEPARATOR . 'ShipsCleaner.php');
+    require(OPBEPATH . 'combatObject' . DIRECTORY_SEPARATOR . 'FireManager.php');
+    require(OPBEPATH . 'core' . DIRECTORY_SEPARATOR . 'Battle.php');
+    require(OPBEPATH . 'core' . DIRECTORY_SEPARATOR . 'BattleReport.php');
+    require(OPBEPATH . 'core' . DIRECTORY_SEPARATOR . 'Round.php');
+    require(OPBEPATH . 'constants' . DIRECTORY_SEPARATOR . 'battle_constants.php');
+}
