6398eb0caba56aad66ff5c1ff5c0dfd35510e834
Lucas Kovacs <lucas.kovacs@gmail.com>
2023-04-12 00:27:40 +0200

    renamed controllers
    I want to get closer to a laravel structure

---
+++ src/app/Http/Controllers/Game/Fleet1Controller.php
@@ -0,0 +1,341 @@
+<?php
+
+namespace App\Controllers\Game;
+
+use App\Core\BaseController;
+use App\Core\Enumerators\ShipsEnumerator as Ships;
+use App\Helpers\UrlHelper;
+use App\Libraries\FleetsLib;
+use App\Libraries\FormatLib;
+use App\Libraries\Functions;
+use App\Libraries\Game\Fleets;
+use App\Libraries\Premium\Premium;
+use App\Libraries\Research\Researches;
+use App\Libraries\Users;
+
+class Fleet1Controller extends BaseController
+{
+    public const MODULE_ID = 8;
+
+    /**
+     *
+     * @var \Fleets
+     */
+    private $_fleets = null;
+
+    /**
+     *
+     * @var \Research
+     */
+    private $_research = null;
+
+    /**
+     *
+     * @var \Premium
+     */
+    private $_premium = null;
+
+    /**
+     *
+     * @var int
+     */
+    private $_ship_count = 0;
+    protected $fleetModel;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        // check if session is active
+        Users::checkSession();
+
+        // load Model
+        parent::loadModel('game/fleet');
+
+        // load Language
+        parent::loadLang(['game/ships', 'game/fleet']);
+
+        // init a new fleets object
+        $this->setUpFleets();
+    }
+
+    public function index(): void
+    {
+        // Check module access
+        Functions::moduleMessage(Functions::isModuleAccesible(self::MODULE_ID));
+
+        // build the page
+        $this->buildPage();
+    }
+
+    /**
+     * Creates a new ships object that will handle all the ships
+     * creation methods and actions
+     *
+     * @return void
+     */
+    private function setUpFleets()
+    {
+        $this->_fleets = new Fleets(
+            $this->fleetModel->getAllFleetsByUserId($this->user['user_id']),
+            $this->user['user_id']
+        );
+
+        $this->_research = new Researches(
+            [$this->user],
+            $this->user['user_id']
+        );
+
+        $this->_premium = new Premium(
+            [$this->user],
+            $this->user['user_id']
+        );
+    }
+
+    private function buildPage(): void
+    {
+        /**
+         * Parse the items
+         */
+        $page = [
+            'js_path' => JS_PATH,
+            'fleets' => $this->_fleets->getFleetsCount(),
+            'max_fleets' => FleetsLib::getMaxFleets(
+                $this->_research->getCurrentResearch()->getResearchComputerTechnology(),
+                $this->_premium->getCurrentPremium()->getPremiumOfficierAdmiral()
+            ),
+            'expeditions' => $this->_fleets->getExpeditionsCount(),
+            'max_expeditions' => FleetsLib::getMaxExpeditions(
+                $this->_research->getCurrentResearch()->getResearchAstrophysics()
+            ),
+            'no_slot' => $this->buildNoSlotBlock(),
+            'list_of_ships' => $this->buildListOfShips(),
+            'none_max_selector' => $this->buildActionsBlock(),
+            'no_ships' => $this->buildNoShipsBlock(),
+            'continue_button' => $this->buildContinueBlock(),
+        ];
+
+        // display the page
+        $this->page->display(
+            $this->template->set(
+                'fleet/fleet1_view',
+                array_merge(
+                    $this->langs->language,
+                    $page,
+                    $this->setInputsData()
+                )
+            )
+        );
+    }
+
+    /**
+     * Build the not slot block
+     *
+     * @return type
+     */
+    private function buildNoSlotBlock()
+    {
+        if (!$this->checkAvailableSlot()) {
+            return $this->template->set('fleet/fleet1_noslots_row', $this->langs->language);
+        }
+
+        return null;
+    }
+
+    /**
+     * Build the list of ships
+     *
+     * @return array
+     */
+    private function buildListOfShips()
+    {
+        $objects = $this->objects->getObjects();
+        $price = $this->objects->getPrice();
+
+        $ships = $this->fleetModel->getShipsByPlanetId($this->planet['planet_id']);
+
+        $list_of_ships = [];
+
+        if ($ships != null) {
+            foreach ($ships as $ship_name => $ship_amount) {
+                if ($ship_amount != 0) {
+                    $this->_ship_count += $ship_amount;
+
+                    $ship_id = array_search($ship_name, $objects);
+
+                    $list_of_ships[] = [
+                        'ship_name' => $this->buildShipName($ship_name, $ship_id),
+                        'ship_amount' => $this->buildShipAmount($ship_amount),
+                        'max_ships_link' => $this->buildMaxShipsLink($ship_id) ?? '-',
+                        'ships_input' => $this->buildShipsInput($ship_id) ?? '-',
+                        'ship_id' => $ship_id,
+                        'max_ships' => $ship_amount,
+                        'consumption' => FleetsLib::shipConsumption($ship_id, $this->user),
+                        'speed' => FleetsLib::fleetMaxSpeed('', $ship_id, $this->user),
+                        'capacity' => FleetsLib::getMaxStorage(
+                            $price[$ship_id]['capacity'],
+                            $this->_research->getCurrentResearch()->getResearchHyperspaceTechnology()
+                        ),
+                    ];
+                }
+            }
+        }
+
+        return $list_of_ships;
+    }
+
+    /**
+     * Build the ship name block
+     *
+     * @param string $ship_name Ship Name
+     * @param int    $ship_id   Ship ID
+     *
+     * @return type
+     */
+    private function buildShipName($ship_name, $ship_id)
+    {
+        $title = $this->langs->line('fl_speed_title') . FleetsLib::fleetMaxSpeed('', $ship_id, $this->user);
+
+        return UrlHelper::setUrl('', $this->langs->line($ship_name), $title);
+    }
+
+    /**
+     * Build the ship amount block
+     *
+     * @param int $ship_amount Ship Amount
+     *
+     * @return string
+     */
+    private function buildShipAmount($ship_amount)
+    {
+        return FormatLib::prettyNumber($ship_amount);
+    }
+
+    /**
+     * Build the ship max link
+     *
+     * @param int $ship_id Ship ID
+     *
+     * @return string
+     */
+    private function buildMaxShipsLink($ship_id)
+    {
+        if ($ship_id == Ships::ship_solar_satellite) {
+            return null;
+        }
+
+        return UrlHelper::setUrl('#', $this->langs->line('fl_max'), '', 'onclick="javascript:maxShip(\'ship' . $ship_id . '\');"');
+    }
+
+    /**
+     * Build the ship input field
+     *
+     * @param int $ship_id Ship ID
+     *
+     * @return string
+     */
+    private function buildShipsInput($ship_id)
+    {
+        if ($ship_id == Ships::ship_solar_satellite) {
+            return null;
+        }
+
+        return '<input name="ship' . $ship_id . '" size="10" value="0" onfocus="javascript:if(this.value == \'0\') this.value=\'\';" onblur="javascript:if(this.value == \'0\') this.value=\'\';"/>';
+    }
+
+    /**
+     * Build the actions block
+     *
+     * @return string
+     */
+    private function buildActionsBlock()
+    {
+        if ($this->_ship_count > 0
+            && $this->checkAvailableSlot()) {
+            return $this->template->set('fleet/fleet1_selector_row', $this->langs->language);
+        }
+
+        return '';
+    }
+
+    /**
+     * Build the no ships block
+     *
+     * @return string
+     */
+    private function buildNoShipsBlock()
+    {
+        if ($this->_ship_count <= 0) {
+            return $this->template->set('fleet/fleet1_noships_row', $this->langs->language);
+        }
+
+        return '';
+    }
+
+    /**
+     * Build the continue button block
+     *
+     * @return string
+     */
+    private function buildContinueBlock()
+    {
+        if ($this->_ship_count > 0
+            && $this->checkAvailableSlot()) {
+            return $this->template->set('fleet/fleet1_button', $this->langs->language);
+        }
+
+        return '';
+    }
+
+    /**
+     * Check if we can send the fleet
+     *
+     * @return boolean
+     */
+    private function checkAvailableSlot()
+    {
+        return (FleetsLib::getMaxFleets(
+            $this->_research->getCurrentResearch()->getResearchComputerTechnology(),
+            $this->_premium->getCurrentPremium()->getPremiumOfficierAdmiral()
+        ) > $this->_fleets->getFleetsCount());
+    }
+
+    /**
+     * Set inputs data
+     *
+     * @return array
+     */
+    private function setInputsData()
+    {
+        $data = filter_input_array(INPUT_GET, [
+            'galaxy' => [
+                'filter' => FILTER_VALIDATE_INT,
+                'options' => ['min_range' => 1, 'max_range' => MAX_GALAXY_IN_WORLD],
+            ],
+            'system' => [
+                'filter' => FILTER_VALIDATE_INT,
+                'options' => ['min_range' => 1, 'max_range' => MAX_SYSTEM_IN_GALAXY],
+            ],
+            'planet' => [
+                'filter' => FILTER_VALIDATE_INT,
+                'options' => ['min_range' => 1, 'max_range' => (MAX_PLANET_IN_SYSTEM + 1)],
+            ],
+            'planettype' => [
+                'filter' => FILTER_VALIDATE_INT,
+                'options' => ['min_range' => 1, 'max_range' => 3],
+            ],
+            'target_mission' => FILTER_VALIDATE_INT,
+        ]);
+
+        // always reset, and define as array
+        $_SESSION['fleet_data'] = [];
+
+        return [
+            'galaxy' => $data['galaxy'] ?? $this->planet['planet_galaxy'],
+            'system' => $data['system'] ?? $this->planet['planet_system'],
+            'planet' => $data['planet'] ?? $this->planet['planet_planet'],
+            'planettype' => $data['planettype'] ?? $this->planet['planet_type'],
+            'target_mission' => $data['target_mission'] ?? 0,
+        ];
+    }
+}
---
+++ src/app/Http/Controllers/Home/RegisterController.php
@@ -0,0 +1,240 @@
+<?php
+
+namespace App\Controllers\Home;
+
+use App\Core\BaseController;
+use App\Libraries\Functions;
+
+class RegisterController extends BaseController
+{
+    private array $available_coords = [];
+    private int $error_id;
+    protected $registerModel;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        // load Model
+        parent::loadModel('home/register');
+
+        // load Language
+        parent::loadLang(['home/register']);
+    }
+
+    public function index(): void
+    {
+        if (Functions::readConfig('reg_enable') != 1) {
+            die(Functions::message($this->langs->line('re_disabled'), 'index.php', '5', false, false));
+        }
+
+        // build the page
+        $this->buildPage();
+    }
+
+    private function buildPage(): void
+    {
+        if ($_POST) {
+            $user_name = $_POST['character'];
+            $user_email = $_POST['email'];
+            $user_password = $_POST['password'];
+
+            if (!$this->runValidations()) {
+                if ($this->error_id != '') {
+                    $url = 'index.php?character=' . $user_name . '&email=' . $user_email . '&error=' . $this->error_id;
+                } else {
+                    $url = 'index.php';
+                }
+
+                Functions::redirect(SYSTEM_ROOT . $url);
+            } else {
+                // start user creation
+                $this->calculateNewPlanetPosition();
+
+                $this->registerModel->createNewUser(
+                    $this->userLibrary,
+                    [
+                        'new_user_name' => $user_name,
+                        'new_user_email' => $user_email,
+                        'new_user_password' => $user_password,
+                    ],
+                    $this->available_coords
+                );
+
+                $new_user = $this->registerModel->getNewUserData();
+
+                // Send Welcome Message to the user if the feature is enabled
+                if (Functions::readConfig('reg_welcome_message')) {
+                    Functions::sendMessage(
+                        $new_user['user_id'],
+                        0,
+                        '',
+                        5,
+                        $this->langs->line('re_welcome_message_from'),
+                        $this->langs->line('re_welcome_message_subject'),
+                        str_replace('%s', $new_user['user_name'], $this->langs->line('re_welcome_message_content'))
+                    );
+                }
+
+                // Send Welcome Email to the user if the feature is enabled
+                if (Functions::readConfig('reg_welcome_email')) {
+                    $this->sendPassEmail($new_user['user_email'], $new_user['user_name'], $user_password);
+                }
+
+                // User login
+                if ($this->userLibrary->userLogin($new_user['user_id'], $new_user['user_hashed_password'])) {
+                    // Redirect to game
+                    Functions::redirect(SYSTEM_ROOT . 'game.php?page=overview');
+                }
+            }
+        }
+
+        // If login fails
+        Functions::redirect('index.php');
+    }
+
+    /**
+     * Send the password by email
+     *
+     * @param string $email_address
+     * @param string $user_name
+     * @param string $password
+     * @return void
+     */
+    private function sendPassEmail(string $email_address, string $user_name, string $password): void
+    {
+        $game_name = Functions::readConfig('game_name');
+
+        $parse = $this->langs->language;
+        $parse['user_name'] = $user_name;
+        $parse['user_pass'] = $password;
+        $parse['game_url'] = GAMEURL;
+        $parse['re_mail_text_part1'] = str_replace('%s', $game_name, $this->langs->line('re_mail_text_part1'));
+        $parse['re_mail_text_part7'] = str_replace('%s', $game_name, $this->langs->line('re_mail_text_part7'));
+
+        $email = $this->template->set(
+            'home/welcome_email_template_view',
+            $parse
+        );
+
+        Functions::sendEmail(
+            $email_address,
+            $this->langs->line('re_mail_register_at') . Functions::readConfig('game_name'),
+            $email,
+            [
+                'mail' => Functions::readConfig('admin_email'),
+                'name' => $game_name,
+            ],
+            'html'
+        );
+    }
+
+    /**
+     * Run validations for the registration fields
+     *
+     * @return boolean
+     */
+    private function runValidations(): bool
+    {
+        $errors = 0;
+
+        if (!Functions::validEmail($_POST['email'])) {
+            $errors++;
+        }
+
+        if (!$_POST['character']) {
+            $errors++;
+        }
+
+        if (strlen($_POST['password']) < 8) {
+            $errors++;
+        }
+
+        if (preg_match("/[^A-z0-9_\-]/", $_POST['character']) == 1) {
+            $errors++;
+        }
+
+        if ($_POST['agb'] != 'on') {
+            $errors++;
+        }
+
+        if ($this->registerModel->checkUser($_POST['character'])) {
+            $errors++;
+            $this->error_id = 1;
+        }
+
+        if ($this->registerModel->checkEmail($_POST['email'])) {
+            $errors++;
+            $this->error_id = 2;
+        }
+
+        return ($errors <= 0);
+    }
+
+    /**
+     * Determine what's going to be the position for the new planet
+     *
+     * @return void
+     */
+    private function calculateNewPlanetPosition(): void
+    {
+        $last_galaxy = Functions::readConfig('lastsettedgalaxypos');
+        $last_system = Functions::readConfig('lastsettedsystempos');
+        $last_planet = Functions::readConfig('lastsettedplanetpos');
+
+        while (true) {
+            for ($galaxy = $last_galaxy; $galaxy <= MAX_GALAXY_IN_WORLD; $galaxy++) {
+                for ($system = $last_system; $system <= MAX_SYSTEM_IN_GALAXY; $system++) {
+                    for ($pos = $last_planet; $pos <= 4; $pos++) {
+                        $planet = mt_rand(4, 12);
+
+                        switch ($last_planet) {
+                            case 1:
+                                $last_planet += 1;
+
+                                break;
+
+                            case 2:
+                                $last_planet += 1;
+
+                                break;
+
+                            case 3:
+                                if ($last_system == MAX_SYSTEM_IN_GALAXY) {
+                                    $last_galaxy += 1;
+                                    $last_system = 1;
+                                    $last_planet = 1;
+
+                                    break;
+                                } else {
+                                    $last_planet = 1;
+                                }
+
+                                $last_system += 1;
+
+                                break;
+                        }
+                        break;
+                    }
+                    break;
+                }
+                break;
+            }
+
+            if (!$this->registerModel->checkIfPlanetExists($galaxy, $system, $planet)) {
+                Functions::updateConfig('lastsettedgalaxypos', $last_galaxy);
+                Functions::updateConfig('lastsettedsystempos', $last_system);
+                Functions::updateConfig('lastsettedplanetpos', $last_planet);
+
+                $this->available_coords = [
+                    'galaxy' => $galaxy,
+                    'system' => $system,
+                    'planet' => $planet,
+                ];
+
+                // break
+                return;
+            }
+        }
+    }
+}
