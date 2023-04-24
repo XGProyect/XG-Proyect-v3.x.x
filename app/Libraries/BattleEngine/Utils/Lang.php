d8d9acb33a2720af5d6079f8726def94296a098b
Lucas Kovacs <lucas.kovacs@gmail.com>
2023-04-24 20:18:58 +0200

    reset folder case - part 2

---
+++ app/Libraries/BattleEngine/Utils/Lang.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Libraries\BattleEngine\Utils;
+
+interface Lang
+{
+    public function getShipName($id);
+
+    public function getAttackersAttackingDescr($amount, $damage);
+
+    public function getDefendersDefendingDescr($damage);
+
+    public function getDefendersAttackingDescr($amount, $damage);
+
+    public function getAttackersDefendingDescr($damage);
+
+    public function getTechs($weaponsTech, $shieldsTech, $armourTech);
+
+    public function getAttackerHasWon();
+
+    public function getDefendersHasWon();
+
+    public function getDraw();
+
+    public function getStoleDescr($metal, $crystal, $deuterium);
+
+    public function getAttackersLostUnits($units);
+
+    public function getDefendersLostUnits($units);
+
+    public function getFloatingDebris($metal, $crystal);
+
+    public function getMoonProb($prob);
+
+    public function getNewMoon($name, $galaxy, $system, $planet);
+}
