5d04aabcf5e6b46516a694c2dd0beb789ec0c85a
Lucas Kovacs <lucas.kovacs@gmail.com>
2023-04-27 07:29:40 +0200

    adjustments

--- app/Libraries/OfficiersLib.php
+++ app/Libraries/OfficiersLib.php
@@ -7,51 +7,21 @@ use App\Libraries\TimingLibrary as Timing;
 
 class OfficiersLib
 {
-    /**
-     * isOfficierActive
-     *
-     * @param int $expire_time Expiration time
-     *
-     * @return int
-     */
-    public static function isOfficierActive($expire_time)
+    public static function isOfficierActive(int $expireTime): int
     {
-        return ($expire_time > time() && $expire_time != 0);
+        return ($expireTime > time() && $expireTime != 0);
     }
 
-    /**
-     * getMaxEspionage
-     *
-     * @param int $espionage_tech    Espionage tech level
-     * @param int $technocrate_level Technocrate level
-     *
-     * @return int
-     */
-    public static function getMaxEspionage($espionage_tech, $technocrate_level)
+    public static function getMaxEspionage(int $espionageTech, int $technocrateLevel): int
     {
-        return $espionage_tech + (1 * (self::isOfficierActive($technocrate_level) ? TECHNOCRATE_SPY : 0));
+        return $espionageTech + (1 * (self::isOfficierActive($technocrateLevel) ? TECHNOCRATE_SPY : 0));
     }
 
-    /**
-     * getMaxComputer
-     *
-     * @param int $computer_tech Computer tech level
-     * @param int $amiral_level  Amiral level
-     *
-     * @return int
-     */
-    public static function getMaxComputer($computer_tech, $amiral_level)
+    public static function getMaxComputer(int $computerTech, int $admiralLevel): int
     {
-        return 1 + $computer_tech + (1 * (self::isOfficierActive($amiral_level) ? AMIRAL : 0));
+        return 1 + $computerTech + (1 * (self::isOfficierActive($admiralLevel) ? AMIRAL : 0));
     }
 
-    /**
-     * Get the officer time left as string
-     *
-     * @param int $expiration
-     * @param array $lang
-     * @return string
-     */
     public static function getOfficierTimeLeft(int $expiration, array $lang): string
     {
         $lang_line = 'of_time_remaining_many';
